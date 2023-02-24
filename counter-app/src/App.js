22b939cd8317af558600bcab735f7e118a374ca5
Muhammad-Saifullah50 <saifullahm2005@gmail.com>
2023-02-24 08:48:32 +0500

    started app

--- counter-app/src/App.js
+++ counter-app/src/App.js
@@ -1,8 +1,16 @@
+// main logic of application is in this file. we will link external components to this.
+
 import React from "react";
 const App = () => {
   return(<>
-     <h1>Hello React</h1>
-     <p>This is para</p>
+     <div className="container">
+       <h1>Counter App</h1>
+
+      <input type="number" />
+      
+      <button>Increment</button>
+      <button>Decrement</button>
+      </div>
   </>)
 };
 
